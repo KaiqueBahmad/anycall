de636e6f691ad77d26d301f64d9d27d0470844a0
Kaique Bahmad <kaiquebahmadt@gmail.com>
2026-05-30 00:29:00 -0300

    feat(docker): adiciona serviços supplier e consumer com healthcheck e rede

--- java/example-consumer/src/main/java/kaiquebt/dev/anycall/example/ConsumerApplication.java
+++ java/example-consumer/src/main/java/kaiquebt/dev/anycall/example/ConsumerApplication.java
@@ -1,7 +1,12 @@
 package kaiquebt.dev.anycall.example;
 
+import kaiquebt.dev.anycall.AnyCallClient;
+import kaiquebt.dev.anycall.example.model.CreateProductRequest;
+import kaiquebt.dev.anycall.example.model.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ConsumerApplication {
@@ -10,4 +15,29 @@ public class ConsumerApplication {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
+	@Component
+	public static class ConsumerRunner implements CommandLineRunner {
+		private final AnyCallClient anyCall;
+
+		public ConsumerRunner(AnyCallClient anyCall) {
+			this.anyCall = anyCall;
+		}
+
+		@Override
+		public void run(String... args) throws Exception {
+			try {
+				System.out.println("[Consumer] Chamando supplier...");
+				long startTime = System.currentTimeMillis();
+				Product response = anyCall.call("create-new-product", new CreateProductRequest("teste", 123), Product.class);
+				long endTime = System.currentTimeMillis();
+				System.out.println("[Consumer] Resposta recebida em " + (endTime - startTime) + "ms");
+				System.out.println("[Consumer] Produto: " + response);
+			} catch (Exception e) {
+				System.err.println("[Consumer] Erro ao chamar supplier: " + e.getMessage());
+				e.printStackTrace();
+			} finally {
+				System.exit(0);
+			}
+		}
+	}
 }
