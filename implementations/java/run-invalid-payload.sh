8b55f02b4415071c1b59d052ffa6af7dca34630b
Kaique Bahmad <kaiquebahmadt@gmail.com>
2026-06-23 17:51:49 -0300

    chore(java): remove obsolete shell scripts for rebuild and run
    These scripts are no longer needed as the build and execution workflow has been migrated to Maven-based commands.

--- implementations/java/run-invalid-payload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-mvn package -q -DskipTests
-java -cp example-consumer/target/anycall-example-consumer.jar dev.kaiquebt.anycall.example.runners.InvalidPayloadClient
