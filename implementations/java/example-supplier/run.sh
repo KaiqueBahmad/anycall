4ef0ca467d5d84f0b71c7240fe841dc8775c1f67
Kaique Bahmad <kaiquebahmadt@gmail.com>
2026-06-23 18:42:24 -0300

    chore(java): remove example-supplier run.sh script

--- implementations/java/example-supplier/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# repo root (parent pom.xml)
-cd "$(dirname "$0")/.."
-
-./example-supplier/mvnw -pl example-supplier -am install -DskipTests
-
-java -jar example-supplier/target/anycall-example-supplier.jar
